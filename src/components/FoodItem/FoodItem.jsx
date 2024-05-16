0c98ede7290e0ae759a8fcc1eae9e5afb5d94721
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-16 09:10:12 +0200

    creating and styling my footer

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,23 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id, name, price,description,image}) => {
     const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
-   const fake = {id : 1}
+   const[itemCount,setItemCount] = useState(0)
 
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
-            <p>sgdfsgdfs</p>
+            
             <img className='food-item-image' src={image} alt={name} />
-            { !fake.id
-                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=""  />
+            { !cartItems
+                ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=""  />
                 : <div className="food-item-counter">
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                    <p>{fake.id}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
+                    <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                    <p>{itemCount}</p>
+                    <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
                 </div>
             }
         </div>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className="footer" id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi ducimus nostrum aperiam dolorum ipsa nisi impedit quas esse quasi!</p>
+                <div className="social-footer-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </div>            
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
