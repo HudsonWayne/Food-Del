dd6d1db3abad55905f004a002323913f15717bd9
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-20 09:23:05 +0200

    git added props so that my login can get clickable

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
 
-export const LoginPopup = (setShowLogin) => {
+export const LoginPopup = (props) => {
 
     const [currState, setCurrState] = useState("Login")
 
@@ -12,7 +12,7 @@ export const LoginPopup = (setShowLogin) => {
         <form className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
-                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+                <img onClick={()=>props.setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
                 <input type="text" placeholder='Your name' required/>
