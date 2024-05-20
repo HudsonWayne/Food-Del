85759eb3c07a3cce2626c5558259e1766e13e204
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-20 15:01:47 +0200

    added my full cart.css

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -22,14 +22,16 @@ const Cart = () => {
             if(cartItems[item._id]>0)
              {
               return(
-                <div key={item._id_}  className="cart-items-title cart-items-item">
-                  <p>{item.name}</p>
+                <div key={item._id_}>
+                  <div key={item._id_}  className="cart-items-title cart-items-item">
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price*cartItems[item._id]}</p>
-                  <p>x</p>
+                  <p className='cross'>x</p>
+                </div>
+                <hr />
                 </div>
               )
              }
