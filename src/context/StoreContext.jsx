3ccc54c4abab887e78dd1db6ebe438b927254a8e
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-21 10:11:50 +0200

    added a cart button

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -3,7 +3,6 @@
 
 // export const StoreContextProvider = createContext(null)
 
-
 // const MyStoreContextProvider = (props) =>{
 //     const contextValue = {
 //         food_list
@@ -24,24 +23,30 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const[cartItems,setCartItems] =useState({});
+  const [cartItems, setCartItems] = useState({});
 
-    const addToCart =(itemId) =>{
-            if(!cartItems[itemId]) {
-                setCartItems((prev)=>({...prev,[itemId]:1}))
-            }
-            else{
-                setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-            }
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    const removeFromCart = (itemId) =>{
-        setCartItems((prev)=>({...prev,[itemId]:-1}))
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: -1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo * cartItems[item];
+      }
     }
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
-
+    return totalAmount
+  };
 
   const contextValue = {
     food_list,
@@ -49,6 +54,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount,
   };
 
   return (
@@ -59,11 +65,6 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
-
-
-
-
-
 
 // import { createContext } from "react";
 // import { food_list } from "../assets/assets";
@@ -82,5 +83,3 @@ export default StoreContextProvider;
 // };
 
 // export default StoreContextProvider;
-
-
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import { StoreContext } from "../../context/StoreContext";
 import { useContext } from "react";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className="cart">
@@ -47,7 +47,7 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>{0}</p>
+              <p>{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
@@ -57,7 +57,7 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{0}</b>
+              <b>{getTotalCartAmount()+2}</b>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
